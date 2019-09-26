773514d2edba8aba1d6371ef074c5dcaf708f91d
Eric Agol <agol@uw.edu>
2019-09-26 12:56:44 -0700

    Added plots of fractional error for transit times & derivatives to test_ttv_elements.jl and ran for 10^3 days

--- test/test_ttv_elements.jl
+++ test/test_ttv_elements.jl
@@ -1,4 +1,4 @@
-#include("../src/ttv.jl")
+  #include("../src/ttv.jl"2
 #include("/Users/ericagol/Computer/Julia/regress.jl")
 
 #@testset "ttv_elements" begin
@@ -13,7 +13,7 @@ t0 = 7257.93115525-7300.0
 h  = 0.04
 #tmax = 600.0
 #tmax = 800.0
-tmax = 100.0
+tmax = 1000.0
 
 # Read in initial conditions:
 elements = readdlm("elements.txt",',')
@@ -42,8 +42,6 @@ count1 = zeros(Int64,n)
 # Call the ttv function:
 rstar = 1e12
 dq = ttv_elements!(n,t0,h,tmax,elements,tt1,count1,0.0,0,0,rstar)
-dq = ttv_elements!(n,t0,h,tmax,elements,tt1,count1,0.0,0,0,rstar)
-dq = ttv_elements!(n,t0,h,tmax,elements,tt1,count1,0.0,0,0,rstar)
 # Now call with half the timestep:
 count2 = zeros(Int64,n)
 count3 = zeros(Int64,n)
@@ -66,8 +64,6 @@ end
 
 # Now, compute derivatives (with respect to initial cartesian positions/masses):
 dtdelements0 = zeros(n,maximum(ntt),7,n)
-dtdelements0 = ttv_elements!(n,t0,h,tmax,elements,tt,count,dtdq0,rstar)
-dtdelements0 = ttv_elements!(n,t0,h,tmax,elements,tt,count,dtdq0,rstar)
 dtdelements0 = ttv_elements!(n,t0,h,tmax,elements,tt,count,dtdq0,rstar)
 dtdq2 = zeros(n,maximum(ntt),7,n)
 dtdelements2 = zeros(n,maximum(ntt),7,n)
@@ -105,11 +101,78 @@ dtdelements0_num = zeros(BigFloat,n,maximum(ntt),7,n)
 # Compute derivatives with BigFloat for additional precision:
 elements0 = copy(elements)
 #delement = big.([1e-15,1e-15,1e-15,1e-15,1e-15,1e-15,1e-15])
-dq0 = big(1e-20)
+#dq0 = big(1e-20)
+dq0 = big(1e-10)
 tt2 = big.(tt2)
 tt3 = big.(tt3)
 t0big = big(t0); tmaxbig = big(tmax); hbig = big(h)
 zilch = big(0.0)
+# Compute the transit times in BigFloat precision:
+tt_big = big.(tt); elementsbig = big.(elements0); rstarbig = big(rstar)
+dqbig = ttv_elements!(n,t0big,hbig,tmaxbig,elementsbig,tt_big,count,big(0.0),0,0,rstarbig)
+
+mask = zeros(Bool, size(dtdq0))
+for jq=1:n_body
+  for iq=1:7
+    if iq == 7; ivary = 1; else; ivary = iq+1; end  # Shift mass variation to end
+    for i=2:n
+      for k=1:count2[i]
+        # Ignore inclination & longitude of nodes variations:
+        if iq != 5 && iq != 6 && ~(jq == 1 && iq < 7) && ~(jq == i && iq == 7)
+          mask[i,k,iq,jq] = true
+        end
+      end
+    end
+  end
+end
+
+# Now, compute derivatives (with respect to initial cartesian positions/masses):
+dtdelements0 = zeros(n,maximum(ntt),7,n)
+dtdelements0 = ttv_elements!(n,t0,h,tmax,elements,tt,count,dtdq0,rstar)
+dtdq2 = zeros(n,maximum(ntt),7,n)
+dtdelements2 = zeros(n,maximum(ntt),7,n)
+dtdelements2 = ttv_elements!(n,t0,h/2.,tmax,elements,tt2,count,dtdq2,rstar)
+dtdq4 = zeros(n,maximum(ntt),7,n)
+dtdelements4 = zeros(n,maximum(ntt),7,n)
+dtdelements4 = ttv_elements!(n,t0,h/4.,tmax,elements,tt4,count,dtdq4,rstar)
+dtdq8 = zeros(n,maximum(ntt),7,n)
+dtdelements8 = zeros(n,maximum(ntt),7,n)
+dtdelements8 = ttv_elements!(n,t0,h/8.,tmax,elements,tt8,count,dtdq8,rstar)
+#println("Maximum error on derivative: ",maximum(abs.(dtdelements0-dtdelements2)))
+#println("Maximum error on derivative: ",maximum(abs.(dtdelements2-dtdelements4)))
+#println("Maximum error on derivative: ",maximum(abs.(dtdelements4-dtdelements8)))
+println("Maximum error on derivative: ",maximum(abs.(asinh.(dtdelements0[mask])-asinh.(dtdelements2[mask]))))
+println("Maximum error on derivative: ",maximum(abs.(asinh.(dtdq0)-asinh.(dtdq2))))
+println("Maximum error on derivative: ",maximum(abs.(asinh.(dtdelements2[mask])-asinh.(dtdelements4[mask]))))
+println("Maximum error on derivative: ",maximum(abs.(asinh.(dtdq2)-asinh.(dtdq4))))
+println("Maximum error on derivative: ",maximum(abs.(asinh.(dtdelements4[mask])-asinh.(dtdelements8[mask]))))
+println("Maximum error on derivative: ",maximum(abs.(asinh.(dtdq4)-asinh.(dtdq8))))
+#read(STDIN,Char)
+
+# Check that this is working properly:
+#for i=1:n
+#  for j=1:count2[i]
+#    println(i," ",j," ",tt[i,j]," ",tt2[i,j]," ",tt[i,j]-tt2[i,j]," ",tt1[i,j]-tt2[i,j])
+#  end
+#end
+#read(STDIN,Char)
+
+# Compute derivatives numerically:
+#nq = 15
+# This "summarizes" best numerical derivative:
+dtdelements0_num = zeros(BigFloat,n,maximum(ntt),7,n)
+
+# Compute derivatives with BigFloat for additional precision:
+elements0 = copy(elements)
+#delement = big.([1e-15,1e-15,1e-15,1e-15,1e-15,1e-15,1e-15])
+#dq0 = big(1e-20)
+dq0 = big(1e-10)
+tt2 = big.(tt2)
+tt3 = big.(tt3)
+t0big = big(t0); tmaxbig = big(tmax); hbig = big(h)
+zilch = big(0.0)
+# Compute the transit times in BigFloat precision:
+tt_big = big(tt); elementsbig = big.(elements0)
 # Now, compute derivatives numerically:
 for jq=1:n_body
   for iq=1:7
@@ -153,7 +216,36 @@ println("Max diff asinh(dtdelements): ",maximum(abs.(asinh.(dtdelements0[mask])-
 #loglog(abs.(reshape(dtdelements0,nderiv)),abs.(reshape(diff_dtdelements0,nderiv)),".")
 #println("Maximum error: ",maximum(diff_dtdelements0))
 
+using PyPlot
 
+# Make a plot of the fractional errors:
+for i=2:3, k=1:7, l=1:3
+  if maximum(abs.(dtdelements0_num[i,2:count1[i],k,l])) > 0
+    loglog(tt[i,2:count1[i]]-tt[i,1],abs.(dtdelements0[i,2:count1[i],k,l]./dtdelements0_num[i,2:count1[i],k,l]-1.))
+  end
+end
+
+clf()
+# Plot the difference in the TTVs:
+for i=2:3
+  diff1 = abs.(tt1[i,2:count1[i]]./tt_big[i,2:count1[i]]-1.0);
+  loglog(tt[i,2:count1[i]]-tt[i,1],diff1);
+end
+for i=2:3, k=1:7, l=1:3
+  if maximum(abs.(dtdelements0_num[i,2:count1[i],k,l])) > 0 && k != 5 && k != 6
+    diff1 = abs.(dtdelements0[i,2:count1[i],k,l]./dtdelements0_num[i,2:count1[i],k,l]-1.);
+    diff2 = abs.(asinh.(dtdelements0[i,2:count1[i],k,l])-asinh.(dtdelements0_num[i,2:count1[i],k,l]));
+    loglog(tt[i,2:count1[i]]-tt[i,1],diff1); 
+#    loglog(tt[i,2:count1[i]]-tt[i,1],diff2,"."); 
+    println(i," ",k," ",l," frac error: ",convert(Float64,maximum(diff1))," asinh error: ",convert(Float64,maximum(diff2))); #read(STDIN,Char);
+  end
+end
+
+# Plot a line that scales as time^{3/2}:
+
+loglog([1.0,1024.0],1e-09*[1,2^15],":")
+loglog([1.0,1024.0],1e-12*[1,2^15],":")
+loglog([1.0,1024.0],1e-15*[1,2^15],":")
 
 #@test isapprox(dtdelements0[mask],dtdelements0_num[mask];norm=maxabs)
 @test isapprox(asinh.(dtdelements0[mask]),asinh.(dtdelements0_num[mask]);norm=maxabs)
