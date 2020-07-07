65e907b80d98cd75e6adb8010c72130fce6ac236
Zach Langford <langfzac@uw.edu>
2020-07-07 14:50:14 -0700

    Edit module file

--- src/NbodyGradient.jl
+++ src/NbodyGradient.jl
@@ -6,10 +6,22 @@ An N-body itegrator that computes derivatives with respect to initial conditions
 
 module NbodyGradient
 
+using LinearAlgebra
+
+# Constants used by most functions
+# Need to clean this up
+const NDIM = 3
+const YEAR = 365.242
+const GNEWT = 39.4845/(YEAR*YEAR)
+const third = 1.0/3.0
+
 include("PreAllocArrays.jl")
-include("integrator.jl")
-include("InitialConditions.jl")
-include("ttv.jl")
+include("utils.jl")
+include("grad/integrator.jl")
+include("nograd/integrator_no_grad.jl")
+include("init_nbody.jl")
+include("nograd/ttv.jl")
+include("grad/ttv.jl")
 
 function ttv_elements!(elems::ElementsIC{T},t0::T,h::T,tmax::T,tt::Array{T,2},count::Array{Int64,1},rstar::T) where T <: AbstractFloat
     elements = elems.elements
