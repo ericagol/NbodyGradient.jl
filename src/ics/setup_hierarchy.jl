181dbb9e6254ee0c82a326b73cfa68140720352e
Zach Langford <langfzac@uw.edu>
2020-08-24 15:18:06 -0700

    Make A-matrix generation consistent with HPZ

--- src/ics/setup_hierarchy.jl
+++ src/ics/setup_hierarchy.jl
@@ -24,7 +24,7 @@ function hierarchy(ic_vec::Array{Int64,1})
     h = zeros(Float64,nbody,nbody)
     # Starts filling the array and returns it
     bottom_level(nbody,bins,h,1)
-    h[end,:] .= 1
+    h[end,:] .= -1
     return h
 end
 
@@ -34,14 +34,14 @@ end
 function bottom_level(nbody::Int64,bins::Array{Int64,1},h::Array{Float64},iter::Int64)
 
     # Fills the very first level of the hierarchy and iterates related variables
-    h[1,1] = 1.0
-    h[1,2] = -1.0
+    h[1,1] = -1.0
+    h[1,2] = 1.0
     if bins[1] > 1
         bins[end] == 1 ? j = 1 : j::Int64 = nbody/2 - 1  
         for i=1:bins[1]-1
             if (j+3) <= nbody
-                h[i+1,j+2] = 1
-                h[i+1,j+3] = -1
+                h[i+1,j+2] = -1
+                h[i+1,j+3] = 1
                 j += 2
             end
         end
@@ -83,12 +83,12 @@ if iter <= length(bins)
     elseif nbody == bodies
         if row == nbody-1
             if (bodies%4) > 2
-                h[row,1:row-2] .= 1
-                h[row,row-1:bodies] .= -1
+                h[row,1:row-2] .= -1
+                h[row,row-1:bodies] .= 1
                 return h
             elseif (bodies%4) <= 2
-                h[row,1:row-1] .= 1
-                h[row,row:bodies] .= -1
+                h[row,1:row-1] .= -1
+                h[row,row:bodies] .= 1
                 return h
             end
         end
@@ -98,42 +98,42 @@ if iter <= length(bins)
     if nbody >= bodies
         if binsp == 1
             if bins[iter] == 1
-                h[row,1:bodies-binsp] .= 1
-                h[row,bodies-binsp + 1:bodies] .= -1
+                h[row,1:bodies-binsp] .= -1
+                h[row,bodies-binsp + 1:bodies] .= 1
                 row = row + binsp
                 binsp = bins[iter]
                 nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             elseif bins[iter] == 2
-                h[row,1:bodies-(2*binsp)-1] .= 1
-                h[row,bodies-(2*binsp)] = -1
-                h[row+1,bodies-(2*binsp)+1] = 1
-                h[row+1,bodies] = -1
+                h[row,1:bodies-(2*binsp)-1] .= -1
+                h[row,bodies-(2*binsp)] = 1
+                h[row+1,bodies-(2*binsp)+1] = -1
+                h[row+1,bodies] = 1
                 row = row + 2
                 binsp = bins[iter]
                 nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             end
         elseif binsp == 2
             if bins[iter] == 1
-                h[row,1:row-1] .= 1
-                h[row,row:bodies] .= -1
+                h[row,1:row-1] .= -1
+                h[row,row:bodies] .= 1
                 row = row + 1
                 binsp = bins[iter]
                 nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             elseif bins[iter] == 2
-                h[row,1:row-1] .= 1
-                h[row,row:bodies-2*(binsp-1)] .= -1
-                h[row+1,bodies-2*(binsp-1)+1] = 1
-                h[row+1,bodies] = -1
+                h[row,1:row-1] .= -1
+                h[row,row:bodies-2*(binsp-1)] .= 1
+                h[row+1,bodies-2*(binsp-1)+1] = -1
+                h[row+1,bodies] = 1
                 row = row + 2
                 binsp = bins[iter]
                 nlevel(nbody,bodies,bins,binsp,row,h,iter+1)
             end
         elseif binsp == 3
             if bins[iter] == 2
-                h[row,1:Int64(bodies/2)-1] .= 1
-                h[row,Int64(bodies/2):2*Int64(bodies/3)] .= -1
-                h[row+1,2*Int64(bodies/3)+1:bodies] .= 1
-                h[row+1,bodies+1] = -1
+                h[row,1:Int64(bodies/2)-1] .= -1
+                h[row,Int64(bodies/2):2*Int64(bodies/3)] .= 1
+                h[row+1,2*Int64(bodies/3)+1:bodies] .= -1
+                h[row+1,bodies+1] = 1
                 row = row + 2
                 binsp = bins[iter]
                 bodies = bodies + 1
@@ -152,13 +152,13 @@ function symmetric(nbody::Int64,bodies::Int64,bins::Array{Int64,1},binsp::Int64,
     #global j = 0
     j = 0
     while row < nbody-1
-        h[row,1+j:2+j] .= 1
-        h[row,j+3:j+4] .= -1
+        h[row,1+j:2+j] .= -1
+        h[row,j+3:j+4] .= 1
         j = j + 4
         row = row + 1
     end
-    h[row,1:binsp] .= 1
-    h[row,binsp+1:nbody] .= -1
+    h[row,1:binsp] .= -1
+    h[row,binsp+1:nbody] .= 1
     #clear!(:j)
 end
 
@@ -167,13 +167,13 @@ function symmetric_nest(nbody,bodies,bins,binsp,row,h,iter)
     j = 0
     while row < nbody-1
         println(j)
-        h[row,1:2+j] .= 1
-        h[row,3+j] =  -1
-        h[row+1,hlf+1:hlf+2+j] .= 1
-        h[row+1,hlf+3+j]  = -1
+        h[row,1:2+j] .= -1
+        h[row,3+j] =  1
+        h[row+1,hlf+1:hlf+2+j] .= -1
+        h[row+1,hlf+3+j]  = 1
         j += 1
         row += 2
     end
-    h[row,1:hlf] .= 1
-    h[row,hlf+1:nbody] .= -1
+    h[row,1:hlf] .= -1
+    h[row,hlf+1:nbody] .= 1
 end
