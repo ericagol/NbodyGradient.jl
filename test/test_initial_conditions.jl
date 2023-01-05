9f86cc67f8ca83c01072d69628fec894b2ef41e3
Zach Langford <langfzac@sas.upenn.edu>
2023-01-05 15:27:42 -0500

    initial conditions tests now test size of elements array and hierarchy specifications for a fixed elements input

--- test/test_initial_conditions.jl
+++ test/test_initial_conditions.jl
@@ -2,9 +2,17 @@
 # or Cartesian coordinates.
 using DelimitedFiles
 
+function compare_ics(ic1::T, ic2::T) where T<:InitialConditions
+    # Compare the fields of the structs (https://stackoverflow.com/questions/62336686/struct-equality-with-arrays)
+    fns = fieldnames(T)
+    f1 = getfield.(Ref(ic1), fns)
+    f2 = getfield.(Ref(ic2), fns)
+    return f1 == f2 
+end
+
 function get_elements_ic_array(t0, H, N)
     fname = "elements.txt"
-    elements = readdlm(fname, ',', comments=true)[1:N,:]
+    elements = readdlm(fname, ',', comments=true)
     ic = ElementsIC(t0, H, elements)
     return ic
 end
@@ -17,7 +25,7 @@ end
 
 function get_elements_ic_elems(t0, H, N)
     fname = "elements.txt"
-    elements = readdlm(fname, ',', comments=true)[1:N,:]
+    elements = readdlm(fname, ',', comments=true)
     elems = Elements{Float64}[]
     for i in 1:N
         push!(elems, Elements(elements[i,:]...))
@@ -34,7 +42,7 @@ end
 
 function get_cartesian_ic_array(t0, N)
     fname = "coordinates.txt"
-    coords = readdlm(fname, ',', comments=true)[1:N,:]
+    coords = readdlm(fname, ',', comments=true)
     ic = CartesianIC(t0, N, coords)
     return ic
 end
@@ -54,12 +62,24 @@ end
 
     # Compare outputs of each method
     tests = Vector{Expr}()
-    fields = [:elements, :amat]
-    for ic1 in ics, ic2 in ics, f in fields
-        ex = :(@test $ic1.$f == $ic2.$f)
+    for ic in ics
+        ex = :(@test compare_ics($(ics[1]), $ic))
         push!(tests, ex)
     end
     return Expr(:block, setup..., tests...)
+end
+
+function run_elements_H_tests(t0, n)
+    H_vec = [n, ones(Int64, n - 1)...]
+    H_mat = NbodyGradient.hierarchy(H_vec)
+
+    ic_n = get_elements_ic_file(t0, n, n)
+    ic_vec = get_elements_ic_file(t0, H_vec, n)
+    ic_mat = get_elements_ic_file(t0, H_mat, n)
+
+    @test compare_ics(ic_n, ic_vec)
+    @test compare_ics(ic_n, ic_mat)
+    return
 end
 
 # Run tests for given H type (int, vector)
@@ -89,17 +109,27 @@ end
     @testset "Elements" begin
         N = 8
         t0 = 7257.93115525 - 7300.0
-        H_vec = [N,ones(Int64, N - 1)...]
-        H_mat = NbodyGradient.hierarchy(H_vec)
 
-        run_elements_tests(t0, H_mat, N)
-        run_elements_tests(t0, H_vec, N) # Test hierarchy vector specification
-        run_elements_tests(t0, N, N)     # Test number-of-bodies specification
+        for n in 2:N
+            H_vec = [n,ones(Int64, n - 1)...]
+            H_mat = NbodyGradient.hierarchy(H_vec)
+
+            # Test each specification method with static hierarchy
+            run_elements_tests(t0, H_mat, n)
+            run_elements_tests(t0, H_vec, n) # Test hierarchy vector specification
+            run_elements_tests(t0, n, n)     # Test number-of-bodies specification
+
+            # Now test each hierarchy method with file method
+            run_elements_H_tests(t0, n)
+        end
     end
 
     @testset "Cartesian" begin
         N = 8
         t0 = 7257.93115525 - 7300.0
-        run_cartesian_tests(t0, N)
+
+        for n in 2:N
+            run_cartesian_tests(t0, n)
+        end
     end
 end
